b4c45e83e2e8c1e20dbfe397c815055f71e28118
Vishakha <tvishakha1610@gmail.com>
2026-05-26 00:55:58 +0530

    Fix browse search

--- Frontend/vite-project/src/components/Browse.jsx
+++ Frontend/vite-project/src/components/Browse.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
-import Job from './Job'
+import React, { useEffect } from 'react';
+import Navbar from './shared/Navbar';
+import Job from './Job';
 import { useDispatch, useSelector } from 'react-redux';
 import { setsearchedQuery } from '@/redux/jobSlice';
 import usegetAllJobs from '@/hooks/usegetAllJobs';
 
-
 const Browse = () => {
     usegetAllJobs();
-    const { allJobs , setsearchedQuery } = useSelector(store => store.job);
+
+    const { allJobs, searchedQuery } = useSelector(
+        (store) => store.job
+    );
+
     const dispatch = useDispatch();
+
     useEffect(() => {
         return () => {
             dispatch(setsearchedQuery(""));
-        }
-    }, [])
+        };
+    }, [dispatch]);
 
-    const filteredJobs = allJobs.filter((job) =>
-    job.title?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
-    job.company?.name?.toLowerCase().includes(searchedQuery.toLowerCase())
-);
+    const filteredJobs = allJobs.filter((job) => {
+        const query = searchedQuery?.toLowerCase() || "";
+
+        return (
+            job?.title?.toLowerCase()?.includes(query) ||
+            job?.company?.name?.toLowerCase()?.includes(query)
+        );
+    });
 
     return (
         <div>
             <Navbar />
+
             <div className='max-w-7xl mx-auto my-10'>
                 <h1>Search Results ({filteredJobs.length})</h1>
 
@@ -36,4 +45,5 @@ const Browse = () => {
         </div>
     );
 };
-export default Browse
+
+export default Browse;
